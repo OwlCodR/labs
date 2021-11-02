74a8a5a0e73ada11a3a104ac6c5a4f23b08ad4ae
OwlCodR <max.lyovkin@mail.ru>
2021-11-02 20:31:07 +0300

    Added CMake, include system with hpps and moved main.cpp from src/

---
+++ semester-3/lab-1/include/Sorter.hpp
@@ -0,0 +1,7 @@
+#ifndef SORTER_HPP
+#define SORTER_HPP
+
+#include "../src/Sorter/Sorter.h"
+#include "../src/Sorter/Sorter.cpp"
+
+#endif
---
+++ semester-3/lab-1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <ctime>
+#include "include/Sorter.hpp"
+#include "../../semester-2/lab-2/include/DynamicArray.hpp"
+#include "../../semester-2/lab-2/include/ArraySequence.hpp"
+#include "../../semester-2/lab-2/include/LinkedList.hpp"
+#include "../../semester-2/lab-2/include/ListSequence.hpp"
+
+
+// This tells Catch2 to provide a main() - do this only once in cpp file
+// #define CATCH_CONFIG_MAIN
+// #include "../include/catch.hpp"
+
+using namespace std;
+
+int comp(int num1, int num2) {
+    if (num1 > num2) 
+        return 1;
+
+    if (num1 < num2) 
+        return -1;
+
+    return 0;
+}
+
+template<class T>
+void setRandomNumbers(ArraySequence<T> &sequence, int count) {
+    for (int i(0); i < count; i++) 
+        sequence.append(rand() % 200 - 100);
+}
+
+template<class T>
+void printSequence(ArraySequence<T>& sequence) {
+    for (int i(0); i < sequence.getSize(); i++) {
+        cout << sequence.get(i) << " ";
+    }
+}
+
+int main()
+{
+    srand(time(nullptr));
+
+    ArraySequence<int> sequence;
+
+    setRandomNumbers(sequence, 100);
+
+    Sorter<int>::quick_sort(&sequence, comp);
+    printSequence(sequence);
+
+    return 0;
+}
